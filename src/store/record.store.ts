20524a0e4705ba994c1f0e17924852db37dbd901
KimJiHoon <kgccm8480@gmail.com>
2024-12-19 22:53:43 +0900

    feat: Post Write 페이지 event handler 추가

--- src/store/record.store.ts
+++ src/store/record.store.ts
@@ -1,15 +1,22 @@
 // store/record.store.ts
 import { create } from 'zustand';
 
+interface Media {
+  mediaId: number;
+  mediaUrl: string;
+}
+
 interface RecordStore {
   title: string;
   content: string;
   tagNames: string[];
   images: File[];
+  medias: Media[]; // 업로드된 이미지 정보
   setTitle: (title: string) => void;
   setContent: (content: string) => void;
   setTagNames: (tags: string[]) => void;
   setImages: (images: File[]) => void;
+  setMedias: (medias: Media[]) => void;
   resetRecord: () => void; // 상태 초기화
 }
 
@@ -18,10 +25,12 @@ const useRecordStore = create<RecordStore>((set) => ({
   content: '',
   tagNames: [],
   images: [],
+  medias: [],
   setTitle: (title) => set(() => ({ title })),
   setContent: (content) => set(() => ({ content })),
   setTagNames: (tagNames) => set(() => ({ tagNames })),
   setImages: (images) => set(() => ({ images })),
+  setMedias: (medias) => set(() => ({ medias })),
   resetRecord: () => set(() => ({ title: '', content: '', tags: [], images: [] })),
 }));
 
